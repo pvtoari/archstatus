797aa7fd6647c8800f0533a8d8916d1be0bd2f99
Ariel Roque <arielroque@protonmail.com>
2025-09-04 00:08:55 +0200

    Implement OS logo header and source

---
+++ include/logo.h
@@ -0,0 +1,46 @@
+#ifndef LOGO_H
+#define LOGO_H
+
+#define LOGO_ARCHLINUX "\
+\n\033[38;2;23;147;209m\x1b[1m          .\x1b[0m\
+\n\033[38;2;23;147;209m\x1b[1m         /#\\     \x1b[0m         #    \033[38;2;23;147;209m | *\x1b[0m\
+\n\033[38;2;23;147;209m\x1b[1m        /###\\    \x1b[0m### #### #### \033[38;2;23;147;209m | | |---. |   | \\ /\x1b[0m\
+\n\033[38;2;23;147;209m\x1b[1m       /p^###\\   \x1b[0m#   #    #  # \033[38;2;23;147;209m | | |   | |   |  X\x1b[0m\
+\n\033[38;2;23;147;209m\x1b[1m      /##P^q##\\  \x1b[0m#   #### #  # \033[38;2;23;147;209m | | |   | ^._.| / \\\x1b[0m\
+\n\033[38;2;23;147;209m\x1b[1m     /##(   )##\\\x1b[0m\
+\n\033[38;2;23;147;209m\x1b[1m    /###P   q#,^\\\x1b[0m     %s\
+\n\033[38;2;23;147;209m\x1b[1m   /P^         ^q\\\x1b[0m"
+
+#define LOGO_ARCHBANG ""
+#define LOGO_ARCHEX ""
+#define LOGO_ARCHMAN ""
+#define LOGO_ARCHSTRIKE ""
+#define LOGO_ARCOLINUX ""
+#define LOGO_ARTIXLINUX ""
+#define LOGO_BLACKARCH ""
+#define LOGO_BLUESTARLINUX ""
+#define LOGO_CHIMERAOS ""
+#define LOGO_CTLOSLINUX ""
+#define LOGO_CRYSTALLINUX ""
+#define LOGO_ENDEAVOUROS ""
+#define LOGO_GARUDALINUX ""
+#define LOGO_HYPERBOLA ""
+#define LOGO_INSTANTOS ""
+#define LOGO_KAOS ""
+#define LOGO_MANJARO ""
+#define LOGO_MSYS2 ""
+#define LOGO_OBARUN ""
+#define LOGO_PARABOLA ""
+#define LOGO_PUPPYRUSA ""
+#define LOGO_REBORNOS ""
+#define LOGO_SNALLINUX ""
+#define LOGO_STEAMOS3 ""
+#define LOGO_SYSTEMRESCUE ""
+#define LOGO_TEARCHLINUX ""
+#define LOGO_UBOS ""
+
+char *get_os_release();
+
+char *get_logo_for(char *os_release);
+
+#endif // LOGO_H
---
+++ src/logo.c
@@ -0,0 +1,49 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "logo.h"
+
+char *get_os_release() {
+    FILE *fp;
+    char buffer[64] = "";
+
+    fp = popen("/usr/bin/lsb_release -ds", "r");
+    fgets(buffer, 64, fp);
+    pclose(fp);
+
+    return strdup(buffer);
+}
+
+char *get_logo_for(char *os_release) {
+#define COMPARE(x) if (strcmp(os_release, x) == 0)
+    COMPARE("\"ArchBang\"") return LOGO_ARCHBANG;
+    COMPARE("\"ArchEx\"") return LOGO_ARCHEX;
+    COMPARE("\"Archman\"") return LOGO_ARCHMAN;
+    COMPARE("\"Arch Linux\"") return LOGO_ARCHLINUX;
+    COMPARE("\"ArchStrike\"") return LOGO_ARCHSTRIKE;
+    COMPARE("\"ArcoLinux\"") return LOGO_ARCOLINUX;
+    COMPARE("\"Artix Linux\"") return LOGO_ARTIXLINUX;
+    COMPARE("\"BlackArch Linux\"") return LOGO_BLACKARCH;
+    COMPARE("\"Bluestar Linux\"") return LOGO_BLUESTARLINUX;
+    COMPARE("\"ChimeraOS\"") return LOGO_CHIMERAOS;
+    COMPARE("\"Ctlos Linux\"") return LOGO_CTLOSLINUX;
+    COMPARE("\"Crystal Linux\"") return LOGO_CRYSTALLINUX;
+    COMPARE("\"EndeavourOS\"") return LOGO_ENDEAVOUROS;
+    COMPARE("\"Garuda Linux\"") return LOGO_GARUDALINUX;
+    COMPARE("\"Hyperbola\"") return LOGO_HYPERBOLA;
+    COMPARE("\"instantOS\"") return LOGO_INSTANTOS;
+    COMPARE("\"KaOS\"") return LOGO_KAOS;
+    COMPARE("\"Manjaro Linux\"") return LOGO_MANJARO;
+    COMPARE("\"MSYS2\"") return LOGO_MSYS2;
+    COMPARE("\"Obarun\"") return LOGO_OBARUN;
+    COMPARE("\"Parabola\"") return LOGO_PARABOLA;
+    COMPARE("\"PuppyRus-A\"") return LOGO_PUPPYRUSA;
+    COMPARE("\"RebornOS\"") return LOGO_REBORNOS;
+    COMPARE("\"Snal Linux\"") return LOGO_SNALLINUX;
+    COMPARE("\"SteamOS 3\"") return LOGO_STEAMOS3;
+    COMPARE("\"SystemRescue\"") return LOGO_SYSTEMRESCUE;
+    COMPARE("\"TeArch Linux\"") return LOGO_TEARCHLINUX;
+    COMPARE("\"UBOS\"") return LOGO_UBOS;
+
+    return LOGO_ARCHLINUX;
+}
