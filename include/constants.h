3d91d34145b10a1a93e913d671513f80e2dbd161
Ariel Roque <arielroque@protonmail.com>
2025-09-15 17:22:11 +0200

    Remove redundant fields from request URL constants

--- include/constants.h
+++ include/constants.h
@@ -6,7 +6,7 @@
 #define MONITOR_ORD_SITE 2
 #define MONITOR_ORD_WIKI 3
 
-#define ARCHLINUX_STATUS_MONITOR_LIST_ENDPOINT  "https://status.archlinux.org/api/getMonitorList/vmM5ruWEAB?page=1&_=1756477778160"
-#define ARCHLINUX_STATUS_EVENT_FEED_ENDPOINT    "https://status.archlinux.org/api/getEventFeed/vmM5ruWEAB?from_date=1753885778&_=1756477778161"
+#define ARCHLINUX_STATUS_MONITOR_LIST_ENDPOINT  "https://status.archlinux.org/api/getMonitorList/vmM5ruWEAB"
+#define ARCHLINUX_STATUS_EVENT_FEED_ENDPOINT    "https://status.archlinux.org/api/getEventFeed/vmM5ruWEAB?from_date=1755355440"
 
 #endif // CONSTANTS_H
